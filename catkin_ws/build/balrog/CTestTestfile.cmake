ee988b0947c6997836178325b8fd938a3da6ac88
fregu856 <fregu856@student.liu.se>
2017-09-22 15:51:21 +0200

    >

---
+++ catkin_ws/build/balrog/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fregu856/TSRT10/catkin_ws/src/balrog
+# Build directory: /home/fregu856/TSRT10/catkin_ws/build/balrog
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
