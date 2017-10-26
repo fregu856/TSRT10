92abd7f6991be46255e596c95100713666ec677b
fregu856 <fregu856@student.liu.se>
2017-10-26 17:12:51 +0200

    Can now start Sauron with a python script.

--- test_code/drone_start.py
+++ test_code/drone_start.py
@@ -1,10 +1,17 @@
+import time
+
 from dronekit import connect, VehicleMode
 
-# Connect to the Vehicle.
+# connect to the Vehicle.
 print "Connecting to vehicle on: /dev/ttyS0"
 vehicle = connect("/dev/ttyS0", wait_ready=True)
 
-# Get some vehicle attributes (state)
+# disable some pre arm checks: (needed for the drone to actually start)
+vehicle.parameters["ARMING_CHECK"] = 2 + 8 + 4096 + 16 + 32 + 64 + 128 + 256 + 512 + 2048 # (no compass)
+#vehicle.parameters["ARMING_CHECK"] = 2 + 16 + 32 + 64 + 128 + 256 + 512 + 2048 # (no compass, no gps)
+print vehicle.parameters["ARMING_CHECK"]
+
+# print all(?) vehicle attributes:
 print "Get some vehicle attribute values:"
 print " GPS: %s" % vehicle.gps_0
 print " Battery: %s" % vehicle.battery
@@ -12,9 +19,7 @@ print " Last Heartbeat: %s" % vehicle.last_heartbeat
 print " Is Armable?: %s" % vehicle.is_armable
 print " System status: %s" % vehicle.system_status.state
 print " Mode: %s" % vehicle.mode.name    # settable
-
 print "##############"
-
 print "Autopilot Firmware version: %s" % vehicle.version
 print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
 print "Global Location: %s" % vehicle.location.global_frame
@@ -38,7 +43,34 @@ print "System status: %s" % vehicle.system_status.state
 print "Mode: %s" % vehicle.mode.name    # settable
 print "Armed: %s" % vehicle.armed    # settable
 
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print "Arming motors"
+    vehicle.mode    = VehicleMode("ALT_HOLD")
+    vehicle.armed   = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print " Waiting for arming..."
+        time.sleep(1)
+
+    print "Taking off!"
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print " Altitude: ", vehicle.location.global_relative_frame.alt
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+            print "Reached target altitude"
+            break
+        time.sleep(1)
+
+arm_and_takeoff(2)
+
 # Close vehicle object before exiting script
 vehicle.close()
-
-print("Completed")
