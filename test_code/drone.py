c339473d00cda130aee6afaab72fdb53a7ea0419
fregu856 <fregu856@student.liu.se>
2017-10-26 14:57:17 +0200

    >

---
+++ test_code/drone.py
@@ -0,0 +1,44 @@
+from dronekit import connect, VehicleMode
+
+# Connect to the Vehicle.
+print "Connecting to vehicle on: /dev/ttyAMA0"
+vehicle = connect("/dev/ttyAMA0", wait_ready=True)
+
+# Get some vehicle attributes (state)
+print "Get some vehicle attribute values:"
+print " GPS: %s" % vehicle.gps_0
+print " Battery: %s" % vehicle.battery
+print " Last Heartbeat: %s" % vehicle.last_heartbeat
+print " Is Armable?: %s" % vehicle.is_armable
+print " System status: %s" % vehicle.system_status.state
+print " Mode: %s" % vehicle.mode.name    # settable
+
+print "##############"
+
+print "Autopilot Firmware version: %s" % vehicle.version
+print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
+print "Global Location: %s" % vehicle.location.global_frame
+print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
+print "Local Location: %s" % vehicle.location.local_frame    #NED
+print "Attitude: %s" % vehicle.attitude
+print "Velocity: %s" % vehicle.velocity
+print "GPS: %s" % vehicle.gps_0
+print "Groundspeed: %s" % vehicle.groundspeed
+print "Airspeed: %s" % vehicle.airspeed
+print "Gimbal status: %s" % vehicle.gimbal
+print "Battery: %s" % vehicle.battery
+print "EKF OK?: %s" % vehicle.ekf_ok
+print "Last Heartbeat: %s" % vehicle.last_heartbeat
+print "Rangefinder: %s" % vehicle.rangefinder
+print "Rangefinder distance: %s" % vehicle.rangefinder.distance
+print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
+print "Heading: %s" % vehicle.heading
+print "Is Armable?: %s" % vehicle.is_armable
+print "System status: %s" % vehicle.system_status.state
+print "Mode: %s" % vehicle.mode.name    # settable
+print "Armed: %s" % vehicle.armed    # settable
+
+# Close vehicle object before exiting script
+vehicle.close()
+
+print("Completed")
