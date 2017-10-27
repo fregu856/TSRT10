98462f105549d6211dcc6c2b68b8cd6752f45967
fregu856 <fregu856@student.liu.se>
2017-10-27 15:31:05 +0200

    >

--- test_code/drone_sim.py
+++ test_code/drone_sim.py
@@ -1,90 +1,146 @@
 import time
 
-print "Start simulator (SITL)"
-import dronekit_sitl
-sitl = dronekit_sitl.start_default()
-connection_string = sitl.connection_string()
+from pymavlink import mavutil
 
-# Import DroneKit-Python
-from dronekit import connect, VehicleMode
+try:
+    print "Start simulator (SITL)"
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
 
-# Connect to the Vehicle.
-print("Connecting to vehicle on: %s" % (connection_string,))
-vehicle = connect(connection_string, wait_ready=True)
+    # Import DroneKit-Python
+    from dronekit import connect, VehicleMode
 
-# Get some vehicle attributes (state)
-print "Get some vehicle attribute values:"
-print " GPS: %s" % vehicle.gps_0
-print " Battery: %s" % vehicle.battery
-print " Last Heartbeat: %s" % vehicle.last_heartbeat
-print " Is Armable?: %s" % vehicle.is_armable
-print " System status: %s" % vehicle.system_status.state
-print " Mode: %s" % vehicle.mode.name    # settable
+    # Connect to the Vehicle.
+    print("Connecting to vehicle on: %s" % (connection_string,))
+    vehicle = connect(connection_string, wait_ready=True)
 
-print "##############"
+    # Get some vehicle attributes (state)
+    print "Get some vehicle attribute values:"
+    print " GPS: %s" % vehicle.gps_0
+    print " Battery: %s" % vehicle.battery
+    print " Last Heartbeat: %s" % vehicle.last_heartbeat
+    print " Is Armable?: %s" % vehicle.is_armable
+    print " System status: %s" % vehicle.system_status.state
+    print " Mode: %s" % vehicle.mode.name    # settable
 
-print "Autopilot Firmware version: %s" % vehicle.version
-print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
-print "Global Location: %s" % vehicle.location.global_frame
-print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
-print "Local Location: %s" % vehicle.location.local_frame    #NED
-print "Attitude: %s" % vehicle.attitude
-print "Velocity: %s" % vehicle.velocity
-print "GPS: %s" % vehicle.gps_0
-print "Groundspeed: %s" % vehicle.groundspeed
-print "Airspeed: %s" % vehicle.airspeed
-print "Gimbal status: %s" % vehicle.gimbal
-print "Battery: %s" % vehicle.battery
-print "EKF OK?: %s" % vehicle.ekf_ok
-print "Last Heartbeat: %s" % vehicle.last_heartbeat
-print "Rangefinder: %s" % vehicle.rangefinder
-print "Rangefinder distance: %s" % vehicle.rangefinder.distance
-print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
-print "Heading: %s" % vehicle.heading
-print "Is Armable?: %s" % vehicle.is_armable
-print "System status: %s" % vehicle.system_status.state
-print "Mode: %s" % vehicle.mode.name    # settable
-print "Armed: %s" % vehicle.armed    # settable
+    print "##############"
 
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
+    print "Autopilot Firmware version: %s" % vehicle.version
+    print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
+    print "Global Location: %s" % vehicle.location.global_frame
+    print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
+    print "Local Location: %s" % vehicle.location.local_frame    #NED
+    print "Attitude: %s" % vehicle.attitude
+    print "Velocity: %s" % vehicle.velocity
+    print "GPS: %s" % vehicle.gps_0
+    print "Groundspeed: %s" % vehicle.groundspeed
+    print "Airspeed: %s" % vehicle.airspeed
+    print "Gimbal status: %s" % vehicle.gimbal
+    print "Battery: %s" % vehicle.battery
+    print "EKF OK?: %s" % vehicle.ekf_ok
+    print "Last Heartbeat: %s" % vehicle.last_heartbeat
+    print "Rangefinder: %s" % vehicle.rangefinder
+    print "Rangefinder distance: %s" % vehicle.rangefinder.distance
+    print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
+    print "Heading: %s" % vehicle.heading
+    print "Is Armable?: %s" % vehicle.is_armable
+    print "System status: %s" % vehicle.system_status.state
+    print "Mode: %s" % vehicle.mode.name    # settable
+    print "Armed: %s" % vehicle.armed    # settable
 
-    print "Basic pre-arm checks"
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print " Waiting for vehicle to initialise..."
-        time.sleep(1)
+    def arm_and_takeoff(aTargetAltitude):
+        """
+        Arms vehicle and fly to aTargetAltitude.
+        """
 
-    print "Arming motors"
-    # Copter should arm in GUIDED mode
-    vehicle.mode    = VehicleMode("GUIDED")
-    vehicle.armed   = True
+        print "Basic pre-arm checks"
+        # Don't try to arm until autopilot is ready
+        while not vehicle.is_armable:
+            print " Waiting for vehicle to initialise..."
+            time.sleep(1)
 
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print " Waiting for arming..."
-        time.sleep(1)
+        print "Arming motors"
+        # Copter should arm in GUIDED mode
+        vehicle.mode    = VehicleMode("GUIDED")
+        vehicle.armed   = True
 
-    print "Taking off!"
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+        # Confirm vehicle armed before attempting to take off
+        while not vehicle.armed:
+            print " Waiting for arming..."
+            time.sleep(1)
 
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
+        print "Taking off!"
+        vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+        # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+        #  after Vehicle.simple_takeoff will execute immediately).
+        while True:
+            print " Altitude: ", vehicle.location.global_relative_frame.alt
+            print "Attitude: %s" % vehicle.attitude
+            #Break and return from function just below target altitude.
+            if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+                print "Reached target altitude"
+                print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+                print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+                print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+                print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+                break
+            time.sleep(1)
+
+    arm_and_takeoff(5)
+
+    def condition_yaw(heading, relative=False):
+        if relative:
+            is_relative=1 #yaw relative to direction of travel
+        else:
+            is_relative=0 #yaw is an absolute angle
+        # create the CONDITION_YAW command using command_long_encode()
+        msg = vehicle.message_factory.command_long_encode(
+            0, 0,    # target system, target component
+            mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+            0, #confirmation
+            heading,    # param 1, yaw in degrees
+            0,          # param 2, yaw speed deg/s
+            1,          # param 3, direction -1 ccw, 1 cw
+            is_relative, # param 4, relative offset 1, absolute angle 0
+            0, 0, 0)    # param 5 ~ 7 not used
+        # send command to vehicle
+        vehicle.send_mavlink(msg)
+
+    def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+        """
+        Move vehicle in direction based on specified velocity vectors.
+        """
+        msg = vehicle.message_factory.set_position_target_local_ned_encode(
+            0,       # time_boot_ms (not used)
+            0, 0,    # target system, target component
+            mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+            0b0000111111000111, # type_mask (only speeds enabled)
+            0, 0, 0, # x, y, z positions (not used)
+            velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+            0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+            0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+
+
+        # send command to vehicle on 1 Hz cycle
+        for x in range(0,duration):
+            vehicle.send_mavlink(msg)
+            time.sleep(1)
+
     while True:
-        print " Altitude: ", vehicle.location.global_relative_frame.alt
-        #Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
-            print "Reached target altitude"
-            break
+        print "Altitude: ", vehicle.location.global_relative_frame.alt
+        print "Velocity: %s" % vehicle.velocity
+        print "Groundspeed: %s" % vehicle.groundspeed
+        print "Airspeed: %s" % vehicle.airspeed
+        send_ned_velocity(1, 1, 1, 1)
         time.sleep(1)
 
-arm_and_takeoff(20)
+    # Close vehicle object before exiting script
+    vehicle.close()
 
-# Close vehicle object before exiting script
-vehicle.close()
-
-# Shut down simulator
-sitl.stop()
-print("Completed")
+    # Shut down simulator
+    sitl.stop()
+    print("Completed")
+except:
+    print "HEJHEJ!"
--- test_code/drone_start.py
+++ test_code/drone_start.py
@@ -1,52 +1,53 @@
-import time
+try:
+    import time
+    from dronekit import connect, VehicleMode
 
-from dronekit import connect, VehicleMode
+    # connect to the Vehicle.
+    print "Connecting to vehicle on: /dev/ttyS0"
+    vehicle = connect("/dev/ttyS0", wait_ready=True)
 
-# connect to the Vehicle.
-print "Connecting to vehicle on: /dev/ttyS0"
-vehicle = connect("/dev/ttyS0", wait_ready=True)
+    vehicle.channels.overrides = {}
 
-# disable some pre arm checks: (needed for the drone to actually start)
-vehicle.parameters["ARMING_CHECK"] = 2 + 8 + 4096 + 16 + 32 + 64 + 128 + 256 + 512 + 2048 # (no compass)
-#vehicle.parameters["ARMING_CHECK"] = 2 + 16 + 32 + 64 + 128 + 256 + 512 + 2048 # (no compass, no gps)
-print vehicle.parameters["ARMING_CHECK"]
+    # disable some pre arm checks: (needed for the drone to actually start)
+    vehicle.parameters["ARMING_CHECK"] = 2 + 8 + 4096 + 16 + 32 + 64 + 128 + 256 + 512 + 2048 # (no compass)
+    #vehicle.parameters["ARMING_CHECK"] = 2 + 16 + 32 + 64 + 128 + 256 + 512 + 2048 # (no compass, no gps)
+    print vehicle.parameters["ARMING_CHECK"]
 
-# print all(?) vehicle attributes:
-print "Get some vehicle attribute values:"
-print " GPS: %s" % vehicle.gps_0
-print " Battery: %s" % vehicle.battery
-print " Last Heartbeat: %s" % vehicle.last_heartbeat
-print " Is Armable?: %s" % vehicle.is_armable
-print " System status: %s" % vehicle.system_status.state
-print " Mode: %s" % vehicle.mode.name    # settable
-print "##############"
-print "Autopilot Firmware version: %s" % vehicle.version
-print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
-print "Global Location: %s" % vehicle.location.global_frame
-print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
-print "Local Location: %s" % vehicle.location.local_frame    #NED
-print "Attitude: %s" % vehicle.attitude
-print "Velocity: %s" % vehicle.velocity
-print "GPS: %s" % vehicle.gps_0
-print "Groundspeed: %s" % vehicle.groundspeed
-print "Airspeed: %s" % vehicle.airspeed
-print "Gimbal status: %s" % vehicle.gimbal
-print "Battery: %s" % vehicle.battery
-print "EKF OK?: %s" % vehicle.ekf_ok
-print "Last Heartbeat: %s" % vehicle.last_heartbeat
-print "Rangefinder: %s" % vehicle.rangefinder
-print "Rangefinder distance: %s" % vehicle.rangefinder.distance
-print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
-print "Heading: %s" % vehicle.heading
-print "Is Armable?: %s" % vehicle.is_armable
-print "System status: %s" % vehicle.system_status.state
-print "Mode: %s" % vehicle.mode.name    # settable
-print "Armed: %s" % vehicle.armed    # settable
+    # print all(?) vehicle attributes:
+    print "Get some vehicle attribute values:"
+    print " GPS: %s" % vehicle.gps_0
+    print " Battery: %s" % vehicle.battery
+    print " Last Heartbeat: %s" % vehicle.last_heartbeat
+    print " Is Armable?: %s" % vehicle.is_armable
+    print " System status: %s" % vehicle.system_status.state
+    print " Mode: %s" % vehicle.mode.name    # settable
+    print "##############"
+    print "Autopilot Firmware version: %s" % vehicle.version
+    print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
+    print "Global Location: %s" % vehicle.location.global_frame
+    print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
+    print "Local Location: %s" % vehicle.location.local_frame    #NED
+    print "Attitude: %s" % vehicle.attitude
+    print "Velocity: %s" % vehicle.velocity
+    print "GPS: %s" % vehicle.gps_0
+    print "Groundspeed: %s" % vehicle.groundspeed
+    print "Airspeed: %s" % vehicle.airspeed
+    print "Gimbal status: %s" % vehicle.gimbal
+    print "Battery: %s" % vehicle.battery
+    print "EKF OK?: %s" % vehicle.ekf_ok
+    print "Last Heartbeat: %s" % vehicle.last_heartbeat
+    print "Rangefinder: %s" % vehicle.rangefinder
+    print "Rangefinder distance: %s" % vehicle.rangefinder.distance
+    print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
+    print "Heading: %s" % vehicle.heading
+    print "Is Armable?: %s" % vehicle.is_armable
+    print "System status: %s" % vehicle.system_status.state
+    print "Mode: %s" % vehicle.mode.name    # settable
+    print "Armed: %s" % vehicle.armed    # settable
 
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
+
+
+
 
     print "Arming motors"
     vehicle.mode    = VehicleMode("ALT_HOLD")
@@ -57,20 +58,34 @@ def arm_and_takeoff(aTargetAltitude):
         print " Waiting for arming..."
         time.sleep(1)
 
-    print "Taking off!"
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
-
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
     #  after Vehicle.simple_takeoff will execute immediately).
+    counter = 0
     while True:
-        print " Altitude: ", vehicle.location.global_relative_frame.alt
-        #Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
-            print "Reached target altitude"
-            break
+        print "##################################################################"
+        print "##################################################################"
+        counter += 1
+        print counter
+
+        print vehicle.channels
+
+        print " Ch1 (Roll): %s" % vehicle.channels["1"]
+        print " Ch2 (Pitch): %s" % vehicle.channels["2"]
+        print " Ch3 (Throttle): %s" % vehicle.channels["3"]
+        print " Ch4 (Yaw): %s" % vehicle.channels["4"]
+
+        if counter > 10:
+            print "$$$$$$$$$$$$$$$$$$$"
+            print "setting pitch!"
+            print "$$$$$$$$$$$$$$$$$$$"
+            vehicle.channels.overrides["2"] = 1600
+
         time.sleep(1)
 
-arm_and_takeoff(2)
 
-# Close vehicle object before exiting script
-vehicle.close()
+
+    # Close vehicle object before exiting script
+    vehicle.close()
+except:
+    print "clear all channel overrides"
+    vehicle.channels.overrides = {}
