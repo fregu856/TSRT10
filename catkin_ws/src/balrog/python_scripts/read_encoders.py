fded7936a30ee367c88ae1b8edb612ed7040d5d4
fregu856 <fregu856@student.liu.se>
2017-11-03 11:25:54 +0100

    Modified read_encoders.py since the RPI doesn't transmit Lidar data anymore.

--- catkin_ws/src/balrog/python_scripts/read_encoders.py
+++ catkin_ws/src/balrog/python_scripts/read_encoders.py
@@ -51,79 +51,74 @@ if __name__ == "__main__":
                 #print "&&&&&&&&&&&&&&&"
 
         if message_type == 1:
-            if len(data) == 136:
+            if len(data) == 128:
                 #print "THE SENSOR DATA HAS ARRIVED!!"
-                lidar = data[0:8]
-                lidar = struct.unpack("<d", lidar)[0]
-                #print "lidar: %f" % lidar
-
-                us_1 = data[8:16]
+                us_1 = data[0:8]
                 us_1 = struct.unpack("<d", us_1)[0]
                 #print "us 1: %f" % us_1
 
-                us_2 = data[16:24]
+                us_2 = data[8:16]
                 us_2 = struct.unpack("<d", us_2)[0]
                 #print "us 2: %f" % us_2
 
-                us_3 = data[24:32]
+                us_3 = data[16:24]
                 us_3 = struct.unpack("<d", us_3)[0]
                 #print "us 3: %f" % us_3
 
-                us_4 = data[32:40]
+                us_4 = data[24:32]
                 us_4 = struct.unpack("<d", us_4)[0]
                 #print "us 4: %f" % us_4
 
-                gyro_1 = data[40:48]
+                gyro_1 = data[32:40]
                 gyro_1 = struct.unpack("<d", gyro_1)[0]
                 #print "gyro 1: %f" % gyro_1
 
-                gyro_2 = data[48:56]
+                gyro_2 = data[40:48]
                 gyro_2 = struct.unpack("<d", gyro_2)[0]
                 #print "gyro 2: %f" % gyro_2
 
-                gyro_3 = data[56:64]
+                gyro_3 = data[48:56]
                 gyro_3 = struct.unpack("<d", gyro_3)[0]
                 #print "gyro 3: %f" % gyro_3
 
-                acc_1 = data[64:72]
+                acc_1 = data[56:64]
                 acc_1 = struct.unpack("<d", acc_1)[0]
                 #print "acc 1: %f" % acc_1
 
-                acc_2 = data[72:80]
+                acc_2 = data[64:72]
                 acc_2 = struct.unpack("<d", acc_2)[0]
                 #print "acc 2: %f" % acc_2
 
-                acc_3 = data[80:88]
+                acc_3 = data[72:80]
                 acc_3 = struct.unpack("<d", acc_3)[0]
                 #print "acc 3: %f" % acc_3
 
-                magn_1 = data[88:96]
+                magn_1 = data[80:88]
                 magn_1 = struct.unpack("<d", magn_1)[0]
                 #print "magn 1: %f" % magn_1
 
-                magn_2 = data[96:104]
+                magn_2 = data[88:96]
                 magn_2 = struct.unpack("<d", magn_2)[0]
                 #print "magn 2: %f" % magn_2
 
-                magn_3 = data[104:112]
+                magn_3 = data[96:104]
                 magn_3 = struct.unpack("<d", magn_3)[0]
                 #print "magn 3: %f" % magn_3
 
-                odoRight = data[112:120]
+                odoRight = data[104:112]
                 odoRight = struct.unpack("<d", odoRight)[0]
                 #print "odoRight: %f" % odoRight
 
-                odoLeft = data[120:128]
+                odoLeft = data[112:120]
                 odoLeft = struct.unpack("<d", odoLeft)[0]
                 #print "odoLeft: %f" % odoLeft
 
-                time = data[128:136]
+                time = data[120:128]
                 time = struct.unpack("<d", time)[0]
                 #print "time: %f" % time
 
-
                 msg = Float64MultiArray()
                 data = [odoRight, odoLeft]
-                #print data
+                print data
                 msg.data = data
                 pub.publish(msg)
